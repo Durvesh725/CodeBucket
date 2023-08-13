3841e222554d531630f24c6fdb45377de9a68220
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-08-13 21:36:13 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -8,18 +8,22 @@ using namespace std;
 class Solution {
     private:
     int mod = 1e9+7;
+    //10 raise to 9
     
     int findFib(int n, vector<int> &dp){
         if(n == 0)  return 0;
         if(n == 1)  return 1;
+        //if solution for the subproblem already exists, return that value directly
         if(dp[n] != -1)
             return dp[n];
             
+        //else store that value in the dp
         return dp[n] = (findFib(n-1, dp) + findFib(n-2, dp)) % mod;
     }
     
   public:
     int nthFibonacci(int n){
+        //initialize 1D DP array
         vector<int> dp(n+1, -1);
         return findFib(n, dp);
     }
