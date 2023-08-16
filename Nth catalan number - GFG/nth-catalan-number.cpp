f9811e73858ecc2630a17a47f97fe236a9c9ee7f
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-08-16 21:16:45 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.cpp
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -15,7 +15,10 @@ class Solution
         vector<long long int> ans(n+1, 0);
         ans[0] = 1;
         ans[1] = 1;
+        //outer loop to maintain the range
         for(int i = 2; i <= n; i++){
+            //Eg: C2 = C0*C1 + C1*C0 
+            //inner while loop helps to satisfy the above formula
             int l = 0, r = i - 1;
             while(l < i && r >= 0){
                 ans[i] = (ans[i] + (ans[l] * ans[r])%mod)%mod;
