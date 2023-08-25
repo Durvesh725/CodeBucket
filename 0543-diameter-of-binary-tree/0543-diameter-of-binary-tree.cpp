c12d2604a8b46989879edd60908778c8ecefa002
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-08-25 18:53:58 +0530

    Time: 9 ms (64.59%), Space: 20.1 MB (80.03%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,36 +11,21 @@
  */
 class Solution {
 private:
-    pair<int, int> fastDiameter(TreeNode* root){
+    int height(TreeNode* root, int &diameter){
         if(root == NULL){
-            pair<int, int> p = make_pair(0, 0);
-            return p;
+            return 0;
         }
-        
-        //now instead of calling height function seperately, 
-        //we'll calculate it along with diameter call
-        pair<int, int> left = fastDiameter(root -> left);
-        pair<int, int> right = fastDiameter(root -> right);
-        
-        //pair.first will store the diameter of the tree 
-        //pair.second will store the height of the tree
-        int op1 = left.first;
-        int op2 = right.first;
-        //calculate height
-        int op3 = left.second + right.second;
-        
-        //store ans
-        pair<int, int> ans;
-        //store max diameter
-        ans.first = max(op1, max(op2, op3));
-        //store max height
-        ans.second = max(left.second, right.second) + 1;
-        
+        int leftHeight = height(root -> left, diameter);
+        int rightHeight = height(root -> right, diameter);
+        diameter = max(diameter, leftHeight + rightHeight);
+        int ans = max(leftHeight, rightHeight) + 1;
         return ans;
     }
 
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        return fastDiameter(root).first;
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
     }
 };
