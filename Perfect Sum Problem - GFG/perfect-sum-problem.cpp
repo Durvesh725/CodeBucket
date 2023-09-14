386b03a22023b093703576d214768fea3f0c6f3f
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-09-14 21:28:08 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    #define mod 1000000007
+    int dfs(int arr[],int ind,int n,int target,vector<vector<int>> &dp){
+        if(ind>=n){
+            if(target==0) return 1;
+             return 0;
+        }
+        if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+        int notTake=dfs(arr,ind+1,n,target,dp)%mod;
+        int take=0;
+        if(target>=arr[ind])
+        {
+            take=dfs(arr,ind+1,n,target-arr[ind],dp)%mod;
+        }
+        
+        return dp[ind][target] = (take+notTake)%mod;
+    }
+    
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        return dfs(arr,0,n,sum,dp);
+	}
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
