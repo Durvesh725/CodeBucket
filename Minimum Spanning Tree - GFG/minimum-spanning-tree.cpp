85f3f5f25966fe41daa872d5583c8b4a87f3816b
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-10-22 21:49:42 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,44 +3,80 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet{
+    vector<int> parent, size;
+    public:
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1);
+        //Initialize parent array
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    //method to find parent: whenever this method is called, the path compression takes place
+    int findUPar(int node){
+        if(node == parent[node])
+            return node;
+        //path compression
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    //UNION BY size
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v)  return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        //if sizes are equal
+        else{
+            parent[ulp_v] = parent[ulp_u];
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        //Priority queue: store node and weight, here we dont require to keep a track of parent as we need
-        // to find sum only and not the MST
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        //visited array
-        vector<int> vis(V, 0);
-        
-        //S1: initially
-        pq.push({0, 0});
-        int sum = 0;
-        
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            
-            int node = it.second;
-            int weight = it.first;
-            //S2: if node is already visited
-            if(vis[node] == 1) continue;
-            
-            vis[node] = 1;
-            //S3: sum
-            sum += weight;
-            //S3: iterate over all the adj nodes of the curr node
-            for(auto it: adj[node]){
+        //Using kruskal's algo
+        //store: {w, {u, v}}} 
+        vector<pair<int, pair<int, int>>> edges;
+        //therefore
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]){
+                int node = i;
                 int adjNode = it[0];
-                int eW = it[1];
-                //S4: check if the particular adj node is already visited
-                if(!vis[adjNode])
-                    pq.push({eW, adjNode});
+                int weight = it[1];
+                edges.push_back({weight, {node, adjNode}});
             }
         }
-        return sum;
+        
+        //2. sort them in increasing order of thier weight
+        sort(edges.begin(), edges.end());
+        
+        //3
+        DisjointSet ds(V);
+        int mstWeight = 0;
+        for(auto it: edges){
+            int w = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            if(ds.findUPar(u) != ds.findUPar(v)){
+                mstWeight += w;
+                ds.unionBySize(u, v);
+            }
+        }
+        return mstWeight;
     }
 };
 
