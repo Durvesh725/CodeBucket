a356031901d67b40831edb081a4de50339e85715
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-10-28 09:39:00 +0530

    Added solution - LeetHub

---
+++ Bleak Numbers - GFG/bleak-numbers.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    private:
+    unsigned int countSetBits(unsigned int n)
+    {
+        unsigned int count = 0;
+        while (n) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+    
+    // A function to return ceiling of log x
+    // in base 2. For example, it returns 3
+    // for 8 and 4 for 9.
+    int ceilLog2(int x)
+    {
+        int count = 0;
+        x--;
+        while (x > 0) {
+            x = x >> 1;
+            count++;
+        }
+        return count;
+    }
+    
+public:
+	int is_bleak(int n)
+	{
+	    for (int x = n - ceilLog2(n); x < n; x++)
+        if (x + countSetBits(x) == n)
+            return false;
+        return true;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
