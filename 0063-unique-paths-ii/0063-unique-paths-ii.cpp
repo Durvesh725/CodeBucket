4f36b20f4349f1ef23b19a337f0cc1a23e096074
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-12-18 19:16:29 +0530

    Time: 5 ms (38.16%), Space: 8.2 MB (45.68%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    int solve(int m, int n, vector<vector<int>> &og, int srcx, int srcy, vector<vector<int>> &dp){
+        //BC
+        if(srcx == m-1 && srcy == n-1){
+            return 1;
+        }
+        
+        if(srcx >= m || srcy >= n){
+            return 0;
+        }
+        
+        if(dp[srcx][srcy] != -1)    return dp[srcx][srcy];
+        
+        //right
+        int right = 0;
+        if(srcy+1 < n && og[srcx][srcy+1] != 1)
+            right = solve(m, n, og, srcx, srcy+1, dp);
+        
+        //down
+        int down = 0;
+        if(srcx+1 < m && og[srcx+1][srcy] != 1)
+            down = solve(m, n, og, srcx+1, srcy, dp);
+        
+        int count = right + down;
+        
+        return dp[srcx][srcy] = count;
+    }
+    
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>&og) {
+        if(og[0][0]==1)
+            return 0;
+        int m = og.size();
+        int n = og[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m, n, og, 0, 0, dp);
+    }
+};
