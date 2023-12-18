2b5b976e7a041c8cd8536aadb336fe0c3b9a1994
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-12-18 10:22:18 +0530

    Time: 21 ms (70.62%), Space: 20.7 MB (28.86%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        int biggest = 0, secondBig = 0, small = INT_MAX, secondSmall = INT_MAX;
+        int biggest = 0, secondBig = 0, small = 1e5,secondSmall = 1e5;
         
-        for(int num: nums){
-            secondBig = (num > biggest) ? exchange(biggest, num) : max(secondBig, num);
-            secondSmall = (num < small) ? exchange(small, num) : min(secondSmall, num);
+        // for(int num: nums){
+        //     secondBig = (num > biggest) ? exchange(biggest, num) : max(secondBig, num);
+        //     secondSmall = (num < small) ? exchange(small, num) : min(secondSmall, num);
+        // }
+        
+        for(int i:nums){
+            small=min(small,i);
+            biggest=max(biggest,i);
+            
         }
+        int sf=0,bf=0;
         
+        for(int i:nums){
+            if(i==small){
+                sf++;
+            }
+            if(i==biggest)
+                bf++;
+          
+            if(i!=small)
+                secondSmall=min(secondSmall,i);
+            if(i!=biggest)
+            secondBig=max(secondBig,i);
+            
+        }
+        if(bf>=2)
+            secondBig=biggest;
+        else if(secondBig==0)
+            secondBig=biggest;
+        if(sf>=2)
+            secondSmall=small;
+        
+        else if(secondSmall==1e5)
+            secondSmall=small;
+        
+        
+            
+        cout<<small<<" "<<secondSmall<<" "<<biggest<<" "<<secondBig;
         return (biggest * secondBig) - (small * secondSmall);
     }
 };
