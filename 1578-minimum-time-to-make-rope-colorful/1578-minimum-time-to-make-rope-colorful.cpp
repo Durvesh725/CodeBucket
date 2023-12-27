5dbbb58f62484be7c54631271b4813cc2848d5ad
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2023-12-27 23:25:33 +0530

    Time: 109 ms (85.10%), Space: 95.8 MB (37.26%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+      int res = 0, max_cost = 0, sum_cost = 0, n = colors.size();
+        //single pass logic
+        for(int i = 0; i < n; i++){
+            //as soon as the color changes, do the previous calculations for the continuous subarray
+            if(i > 0 && colors[i] != colors[i-1]){
+                res += sum_cost - max_cost;
+                sum_cost = 0;
+                max_cost = 0;
+            }
+            sum_cost += neededTime[i];
+            max_cost = max(max_cost, neededTime[i]);
+        }
+        res += sum_cost - max_cost;
+        return res;
+    }
+};
