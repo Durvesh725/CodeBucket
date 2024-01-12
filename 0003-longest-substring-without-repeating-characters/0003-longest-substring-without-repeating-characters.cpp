78fac40852ed77e6d90f0676f7b26e2419650732
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-01-12 17:31:31 +0530

    Time: 14 ms (60.61%), Space: 10.6 MB (39.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //frequency map
-        vector<int> mp(256, -1);
-        int left = 0, right = 0;
+//         //frequency map
+//         vector<int> mp(256, -1);
+//         int left = 0, right = 0;
+//         int len = 0;
+//         int n = s.length();
+//         while(right < n){
+//             //if pos of particular char already exists in a map
+//             if(mp[s[right]] != -1)
+//                 left = max(mp[s[right]] + 1, left);
+            
+//             mp[s[right]] = right;
+            
+//             len = max(len, right-left+1);
+//             right++;
+//         }
+//         return len;
+        
+        //set approach
         int len = 0;
-        int n = s.length();
-        while(right < n){
-            //if pos of particular char already exists in a map
-            if(mp[s[right]] != -1)
-                left = max(mp[s[right]] + 1, left);
+        int left = 0;
+        set<char> ss;
+        
+        for(int i = 0; i < s.size(); i++){
+            int currSize = ss.size();            
+            ss.insert(s[i]);
             
-            mp[s[right]] = right;
+            //duplicate element has occured
+            if(currSize == ss.size()){
+                while(s[left] != s[i]){
+                    ss.erase(s[left]);
+                    left++;   
+                }   
+                
+                left++;
+            }
             
-            len = max(len, right-left+1);
-            right++;
+            len = max(len, i - left + 1);
         }
         return len;
     }
