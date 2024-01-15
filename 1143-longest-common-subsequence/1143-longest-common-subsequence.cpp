92a34209a3acd403ce866525289fe07fdce8fcd9
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-01-15 09:24:49 +0530

    Time: 56 ms (37.57%), Space: 25.1 MB (10.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,27 @@
 class Solution {
-    //using top-down DP approch
-    private:
-    int solve(string &s1, string &s2, int i, int j, vector<vector<int>>& dp){
-        //base case
-        if(i == s1.length())
-            return 0;
-        if(j == s2.length())
+private:
+    int solve(int ind1, int ind2, string &text1, string &text2, vector<vector<int>> &dp){
+        if(ind1 < 0 || ind2 < 0)
             return 0;
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
+        if(dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
         
-        int ans = 0;
-        if(s1[i] == s2[j]){
-            ans = 1 + solve(s1, s2, i+1, j+1, dp);
+        //if both the character matches
+        if(text1[ind1] == text2[ind2]){
+            return 1 + solve(ind1-1, ind2-1, text1, text2, dp);
         }
-        else{
-            ans = max(solve(s1, s2, i+1, j, dp), solve(s1, s2, i, j+1, dp));
-        }
-        return dp[i][j] = ans;
+        
+        //if not matches, try out both the combinations
+        return dp[ind1][ind2] = 0 + max(solve(ind1-1, ind2, text1, text2, dp), solve(ind1, ind2-1, text1, text2, dp));
     }
+    
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        //create 2d DP
-        vector<vector<int> > dp(text1.length(), vector<int>(text2.length(), -1));
-        return solve(text1, text2, 0, 0, dp);   
+        int n = text1.length();
+        int m = text2.length();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(n-1, m-1, text1, text2, dp);
     }
 };
