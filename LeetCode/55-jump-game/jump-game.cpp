7ab1063eb2f708bd7b7e574540368ecd7abf1ba0
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-02-14 22:38:20 +0530

    Time: 41 ms (84.17%) | Memory: 50.8 MB (43.18%) - LeetSync

---
+++ LeetCode/55-jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int lp = n - 1;
+        for(int i = n-1; i >= 0; i--){
+            if(i+nums[i] >= lp)
+                lp = i;
+        }
+        return lp == 0;
+    }
+};
