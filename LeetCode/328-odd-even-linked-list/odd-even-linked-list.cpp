243feaf755d07e6c6c0b00590df6621132fb800a
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-03-08 15:15:20 +0530

    Time: 12 ms (19.55%) | Memory: 14 MB (62.47%) - LeetSync

--- LeetCode/328-odd-even-linked-list/odd-even-linked-list.cpp
+++ LeetCode/328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,34 +11,52 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+        // if(head == NULL || head -> next == NULL)
+        //     return head;
+
+        // vector<int> a;
+
+        // //odd
+        // ListNode* temp = head;
+        // while(temp && temp -> next){
+        //     a.push_back(temp -> val);
+        //     temp = temp -> next -> next;
+        // }
+        // if(temp) a.push_back(temp -> val);
+
+        // temp = head -> next;
+        // while(temp && temp -> next){
+        //     a.push_back(temp -> val);
+        //     temp = temp -> next -> next;
+        // }
+        // if(temp) a.push_back(temp -> val);
+
+        // //replace
+        // temp = head;
+        // int i = 0;
+        // while(temp){
+        //     temp -> val = a[i];
+        //     i++;
+        //     temp = temp -> next;
+        // }
+        // return head;
+
         if(head == NULL || head -> next == NULL)
             return head;
-            
-        vector<int> a;
 
-        //odd
-        ListNode* temp = head;
-        while(temp && temp -> next){
-            a.push_back(temp -> val);
-            temp = temp -> next -> next;
-        }
-        if(temp) a.push_back(temp -> val);
+        ListNode* odd = head;
+        ListNode* even = head -> next;
+        ListNode* evenHead = head -> next;
 
-        temp = head -> next;
-        while(temp && temp -> next){
-            a.push_back(temp -> val);
-            temp = temp -> next -> next;
-        }
-        if(temp) a.push_back(temp -> val);
+        while(even && even -> next){
+            odd -> next = odd -> next -> next;
+            even -> next = even -> next -> next;
 
-        //replace
-        temp = head;
-        int i = 0;
-        while(temp){
-            temp -> val = a[i];
-            i++;
-            temp = temp -> next;
+            odd = odd -> next;
+            even = even -> next;
         }
+
+        odd -> next = evenHead;
         return head;
     }
 };
