bd6e0eebf814ae2521f19937bfa7bc8f14f526ca
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-03-17 16:27:09 +0530

    Time: 287 ms (100.00%) | Memory: 137.1 MB (100.00%) - LeetSync

---
+++ LeetCode/3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<long long> unmarkedSumArray(vector<int>& nums, vector<vector<int>>& queries) {
+        long long sum = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            pq.push({nums[i], i});
+        }
+
+        // while (!pq.empty()) {
+        //     pair<int, int> topPair = pq.top();
+        //     cout << "(" << topPair.first << ", " << topPair.second << ") ";
+        //     pq.pop();
+        // }
+
+        vector<long long> ans;
+        for(auto it: queries){
+            sum -= nums[it[0]];
+            nums[it[0]] = 0;
+            while(pq.size() && it[1]){
+                if(nums[pq.top().second]){
+                    sum -= nums[pq.top().second];
+                    nums[pq.top().second] = 0;
+                    it[1]--;
+                }
+                pq.pop();
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
