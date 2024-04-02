337c5fca26206591647c84f75460bf88515c0b0d
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-02 23:04:28 +0530

    Time: 6 ms (65.28%) | Memory: 8 MB (98.86%) - LeetSync

---
+++ LeetCode/205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        char maps[128] = {0};
+        char mapt[128] = {0};
+        for(int i = 0; i < s.size(); i++){
+            if(maps[s[i]] != mapt[t[i]])
+                return false;
+            
+            maps[s[i]] = i+1;
+            mapt[t[i]] = i+1;
+        }
+        return true;
+    }
+};
