ab936138d853560b570b3824dcf32192dbf294a5
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-05 09:47:37 +0530

    Time: 3 ms (48.39%) | Memory: 8.9 MB (8.61%) - LeetSync

--- LeetCode/1666-make-the-string-great/make-the-string-great.cpp
+++ LeetCode/1666-make-the-string-great/make-the-string-great.cpp
@@ -3,13 +3,14 @@ public:
     string makeGood(string s) {
         stack<char> st;
         for(char c: s){
-            if(!st.empty() && abs(c - st.top()) == 32){
+            if(!st.empty() && ((c <= 'z' && c >= 'a' && st.top() >= 'A' && st.top() <= 'Z') || (st.top() <= 'z' && st.top() >= 'a' && c >= 'A' && c <= 'Z')) && tolower(c) == tolower(st.top()) ){
                 st.pop();
             }
             else{
                 st.push(c);
             }
         }
+
 
         string ans;
         while(!st.empty()){
