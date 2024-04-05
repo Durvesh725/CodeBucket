06b3a83e04f58ad38faed708048b95ce6b840215
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-05 09:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.50%) - LeetSync

--- LeetCode/1666-make-the-string-great/make-the-string-great.cpp
+++ LeetCode/1666-make-the-string-great/make-the-string-great.cpp
@@ -3,7 +3,8 @@ public:
     string makeGood(string s) {
         stack<char> st;
         for(char c: s){
-            if(!st.empty() && ((c <= 'z' && c >= 'a' && st.top() >= 'A' && st.top() <= 'Z') || (st.top() <= 'z' && st.top() >= 'a' && c >= 'A' && c <= 'Z')) && tolower(c) == tolower(st.top()) ){
+            // if(!st.empty() && ((c <= 'z' && c >= 'a' && st.top() >= 'A' && st.top() <= 'Z') || (st.top() <= 'z' && st.top() >= 'a' && c >= 'A' && c <= 'Z')) && tolower(c) == tolower(st.top()))
+            if(!st.empty() && abs(c - st.top()) == 32){
                 st.pop();
             }
             else{
