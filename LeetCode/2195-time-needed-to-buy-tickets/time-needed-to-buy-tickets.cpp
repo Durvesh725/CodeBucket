f94d9583f356a22d51eebe83bc29a034547f0f50
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-09 16:36:05 +0530

    Time: 4 ms (41.19%) | Memory: 9.3 MB (67.56%) - LeetSync

--- LeetCode/2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ LeetCode/2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& t, int k) {
-        queue<int> q;
-        int n = t.size();
-        for(int i = 0; i < t.size(); i++)   q.push(i);
+        // queue<int> q;
+        // int n = t.size();
+        // for(int i = 0; i < t.size(); i++)   q.push(i);
 
+        // int time = 0;
+        // while(!q.empty()){
+        //     time++;
+        //     int ind = q.front();
+        //     q.pop();
+
+        //     t[ind]--;
+        //     if(ind == k && t[ind] == 0) return time;
+        //     if(t[ind] != 0)  q.push(ind);
+        // }
+        // return time;
+
+        //intuition: do a dry run for all passes and observer left and right of kth position
         int time = 0;
-        while(!q.empty()){
-            time++;
-            int ind = q.front();
-            q.pop();
-
-            t[ind]--;
-            if(ind == k && t[ind] == 0) return time;
-            if(t[ind] != 0)  q.push(ind);
+        int n = t.size();
+        for(int i = 0; i < n; i++){
+            if(i <= k)  time += min(t[i], t[k]);
+            else time += min(t[i], t[k]-1);
         }
         return time;
     }
+    //TC: N*max(tickets[i]);
 };
