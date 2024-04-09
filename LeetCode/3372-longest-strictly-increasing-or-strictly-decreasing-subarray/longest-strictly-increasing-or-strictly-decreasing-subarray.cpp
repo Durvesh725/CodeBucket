c3de6dbf527f9983b7a99839406006767df34c96
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-09 18:28:21 +0530

    Time: 3 ms (84.98%) | Memory: 26.7 MB (25.85%) - LeetSync

---
+++ LeetCode/3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& arr) {
+        int c1 = 1, c2 = 1;
+        int mc1 = INT_MIN, mc2 = INT_MIN;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] > arr[i-1]){
+                c1++;
+                mc1 = max(mc1, c1);
+            } 
+            else    c1 = 1;
+        }  
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] < arr[i-1]){
+                c2++;
+                mc2 = max(mc2, c2);
+            }   
+            else    c2 = 1;
+        }   
+        if(mc1 == INT_MIN && mc2 == INT_MIN)
+            return max(c1, c2);
+            
+        return max(mc1, mc2);
+    }
+};
