5946dbe3d71810f2706ca8e138fb0ebe9fd4ffba
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-04-25 11:37:18 +0530

    Time: 512 ms (17.65%) | Memory: 194.1 MB (22.69%) - LeetSync

---
+++ LeetCode/2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int solve(int ind, char prev, string &s, int &k, int &n, vector<vector<int>> &dp){
+        if(ind == n)
+            return 0;
+
+        if(dp[ind][prev-'a'] != -1)
+            return dp[ind][prev-'a'];
+
+        int take = 0, notTake = 0;
+        notTake = solve(ind + 1, prev, s, k, n, dp);
+        if(prev == '{' || abs(s[ind] - prev) <= k){
+            take = 1 + solve(ind + 1, s[ind], s, k, n, dp);
+        }
+        
+        return dp[ind][prev-'a'] = max(take, notTake);
+    }
+
+public:
+    int longestIdealString(string s, int k) {
+        int ind = 0;
+        int n = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(27, -1));
+        return solve(ind, '{', s, k, n, dp);
+    }
+};
