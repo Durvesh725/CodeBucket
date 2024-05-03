b6cd9b34056aa7b248779349357072638195006c
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-05-03 22:00:54 +0530

    Time: 3 ms (39.03%) | Memory: 7.8 MB (12.99%) - LeetSync

---
+++ LeetCode/165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    //custom string split function
+    vector<int> splitString(char delimiter, string &s){
+        vector<int> ans;
+        int start = 0;
+        int end = s.find(delimiter);
+        while(end != string::npos){
+            string substr = s.substr(start, end-start);
+            ans.push_back(stoi(substr));
+            start = end + 1;
+            end = s.find(delimiter, start);
+        }
+
+        string temp = s.substr(start, end-start);
+        ans.push_back(stoi(temp));
+        return ans;
+    }
+
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> n1 = splitString('.', version1);
+        vector<int> n2 = splitString('.', version2);
+        int n1s = n1.size();
+        int n2s = n2.size();
+        int val1, val2;
+        for(int i = 0; i < max(n1s, n2s); i++){
+            val1 = (i < n1s) ? n1[i] : 0;
+            val2 = (i < n2s) ? n2[i] : 0;
+
+            if(val1 < val2)  return -1;
+            else if(val1 > val2) return 1;
+        }
+        return 0;
+    }
+};
