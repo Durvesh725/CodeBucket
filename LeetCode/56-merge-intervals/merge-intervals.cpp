0335a4947be1ee9d1e337bdacf5c317c17fcaf4e
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-06-11 10:15:11 +0530

    Time: 25 ms (45.84%) | Memory: 23.8 MB (13.32%) - LeetSync

--- LeetCode/56-merge-intervals/merge-intervals.cpp
+++ LeetCode/56-merge-intervals/merge-intervals.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> mergedIntervals;
+        vector<vector<int>> ans;
         vector<int> tempInterval = intervals[0];
+
         for(auto it: intervals){
-            //check if overlapping
             if(it[0] <= tempInterval[1]){
                 tempInterval[1] = max(it[1], tempInterval[1]);
             }
             else{
-                mergedIntervals.push_back(tempInterval);
+                ans.push_back(tempInterval);
                 tempInterval = it;
             }
-        }    
-        mergedIntervals.push_back(tempInterval);
-        return mergedIntervals;  
+        }
+
+        ans.push_back(tempInterval);
+
+        return ans;
     }
 };
