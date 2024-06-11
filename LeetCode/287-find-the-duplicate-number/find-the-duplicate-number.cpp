2ebd8123cf77a9c0b16e6d69716cdd738d4d4fc7
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-06-11 12:30:13 +0530

    Time: 70 ms (85.23%) | Memory: 63.6 MB (76.07%) - LeetSync

--- LeetCode/287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ LeetCode/287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // set<int> s;
-        // int dup = 0;
-        // for(auto it: nums){
-        //     if(s.find(it) != s.end())
-        //         dup = it;
-        //     s.insert(it);
-        // }
-        int n = nums.size();
-        while(nums[nums[0]] != nums[0]){
-            swap(nums[0],nums[nums[0]]);
+        //linked list cycle detection algorithm
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        // int bit  = 1;
-        // int  n = nums.size();
-        // for(int i = 0; i < n ; i++){
-        //     bit = bit ^ nums[i];
-        // }
-
-       
-        
-        return nums[0];
+        return slow;
     }
 };
 
