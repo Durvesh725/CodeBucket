6f3add1f7487b9018802325b0849134702a05e5a
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-06-13 18:31:09 +0530

    Time: 9 ms (75.54%) | Memory: 10.5 MB (74.34%) - LeetSync

--- LeetCode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ LeetCode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        vector<int> mp(256, -1);
         int n = s.size();
+        int left = 0, right = 0;
         int maxLen = 0;
 
-        for(int i = 0; i < n; i++){
-            int hash[128] = {0};
-            for(int j = i; j < n; j++){
-                if(hash[s[j]] == 1) break;
-                maxLen = max(maxLen, j - i + 1);
+        while(right < n){
+            //seen char before
+            if(mp[s[right]] != -1)
+                left = max(left, mp[s[right]] + 1);
 
-                hash[s[j]] = 1;
-            }
+            mp[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
 
         return maxLen;
