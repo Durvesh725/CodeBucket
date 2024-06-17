3865a415788fac8ea1c9cfb5ef4553264f6c9ffc
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-06-17 22:32:29 +0530

    Time: 45 ms (7.45%) | Memory: 7 MB (61.35%) - LeetSync

--- LeetCode/633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ LeetCode/633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        for(long long a = 0; a*a <= c; a++){
-            long long b = sqrt(c - a*a);
-            if(a*a + b*b == c)
-                return true;
+        for(long a = 0; a*a <= c; a++){
+            long x = c - a*a;
+            int low = 0, high = x;
+            while(low <= high){
+                long mid = (low+high)/2;
+                if(mid*mid == x)
+                    return true;
+                else if(mid*mid > x)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
         }
         return false;
     }
