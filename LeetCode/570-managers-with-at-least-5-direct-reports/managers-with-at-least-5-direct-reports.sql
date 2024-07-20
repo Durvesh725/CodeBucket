81a04042084ca92699b6b6deb03e6c8bc0fd79a1
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-07-20 11:04:27 +0530

    Time: 369 ms (45.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select name from employee where id IN(
+    select managerId
+    from employee
+    group by managerId
+    having COUNT(managerId) >= 5
+)
