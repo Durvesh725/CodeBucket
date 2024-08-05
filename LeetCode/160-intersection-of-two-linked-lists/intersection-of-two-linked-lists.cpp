33e38e928e07f1f76eda66f492ebd6ffffd17a37
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-05 18:05:08 +0530

    Time: 33 ms (87.32%) | Memory: 17.4 MB (39.87%) - LeetSync

--- LeetCode/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ LeetCode/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,20 +8,44 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> st;
-        ListNode* temp = headA;
+    int getLength(ListNode* head){
+        ListNode* temp = head;
+        int len = 0;
         while(temp){
-            st.insert(temp);
+            len++;
             temp = temp -> next;
         }
+        return len;
+    }
 
-        temp = headB;
-        while(temp){
-            if(st.find(temp) != st.end()){
-                return temp;
-            }
-            temp = temp -> next;
+    ListNode* findCollision(ListNode* headA, ListNode* headB, int d){
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while(d){
+            d--;
+            temp2 = temp2 -> next;
+        }
+
+        while(temp1 && temp2){
+            if(temp1 == temp2)
+                return temp1;
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
+        }
+
+        return NULL;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int l1 = getLength(headA);
+        int l2 = getLength(headB);
+
+        if(l1 < l2){
+            return findCollision(headA, headB, l2-l1);
+        }
+        else{
+            return findCollision(headB, headA, l1-l2);
         }
 
         return NULL;
