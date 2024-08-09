25c9f17e3538be6a5af30a36ae0f7d215752791b
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-09 12:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (52.94%) - LeetSync

---
+++ LeetCode/870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        unordered_set<int> st;
+
+        if(n < 3 || m < 3)
+            return 0;
+
+        const int sum = 15;
+
+        for(int l = 0; l <= n - 3; l++){
+            for(int k = 0; k <= m - 3; k++){
+
+                st.clear();
+                bool isMagic = true;
+
+                for(int i = 0; i < 3; i++){
+                    int s = 0;
+                    for(int j = 0; j < 3; j++){
+                        int val = grid[l + i][k + j];
+                        if(val < 1 || val > 9 || st.count(val)){
+                            isMagic = false;
+                            break;
+                        }
+
+                        st.insert(val);
+                        s += val;
+                    }
+
+                    if(s != sum){
+                        isMagic = false;
+                        break;
+                    }
+                }
+
+                
+                for(int j = 0; j < 3; j++){
+                    int s = 0;
+                    for(int i = 0; i < 3; i++){
+                        s += grid[l + i][k + j];
+                    }
+
+                    if(s != sum){
+                        isMagic = false;
+                        break;
+                    }
+                }
+
+                
+                if(grid[l][k] + grid[l + 1][k + 1] + grid[l + 2][k + 2] != sum ||
+                   grid[l][k + 2] + grid[l + 1][k + 1] + grid[l + 2][k] != sum){
+                    isMagic = false;
+                }
+
+                if(isMagic && st.size() == 9) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
