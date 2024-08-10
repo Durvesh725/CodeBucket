ba72911a3318d81ba5f982db874b3ad53b4b1186
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-10 13:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (36.48%) - LeetSync

--- LeetCode/232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ LeetCode/232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,6 +1,8 @@
 class MyQueue {
 public:
-    stack<int> s1, s2;
+    stack<int> s1;
+    stack<int> s2;
+
     MyQueue() {
         
     }
@@ -10,28 +12,35 @@ public:
             s2.push(s1.top());
             s1.pop();
         }
+
         s1.push(x);
+
         while(!s2.empty()){
             s1.push(s2.top());
             s2.pop();
-        }
+        }    
     }
     
     int pop() {
-        int x = s1.top();
-        s1.pop();
-        return x;
+        if(!s1.empty()){
+            int val = s1.top();
+            s1.pop();
+            return val;
+        }
+        return -1;
     }
     
     int peek() {
-        if(!s1.empty())
+        if(!s1.empty()){
             return s1.top();
+        }
         return -1;
     }
     
     bool empty() {
-        if(s1.empty())
+        if(s1.empty()){
             return true;
+        }
         return false;
     }
 };
