c3b86ef7a8d90909902dcf2539352840a194685d
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-11 17:53:44 +0530

    Time: 3 ms (82.92%) | Memory: 12.5 MB (15.07%) - LeetSync

--- LeetCode/496-next-greater-element-i/next-greater-element-i.cpp
+++ LeetCode/496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,18 +3,22 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> um;
         stack<int> st;
-        for(int i = 0; i < nums2.size(); i++){
+        int n2 = nums2.size();
+        
+        for(int i = 0; i < n2; i++){
             while(!st.empty() && st.top() < nums2[i]){
                 um[st.top()] = nums2[i];
                 st.pop();
             }
+
             st.push(nums2[i]);
         }
 
-        for(int i = 0; i < nums1.size(); i++){
-            nums1[i] = (um.count(nums1[i])) ? um[nums1[i]] : -1;
+        int n1 = nums1.size();
+        for(int i = 0; i < n1; i++){
+            nums1[i] = (um.count(nums1[i])) ? um[nums1[i]] : -1; 
         }
-        
+
         return nums1;
     }
 };
