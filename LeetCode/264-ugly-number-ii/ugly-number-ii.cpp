8039da082f0a2408233be365cd65fa7e096d6aee
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-18 12:40:56 +0530

    Time: 93 ms (13.21%) | Memory: 34.1 MB (13.67%) - LeetSync

---
+++ LeetCode/264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        priority_queue<long, vector<long>, greater<long>> pq; 
+        unordered_set<int> vis;
+
+        pq.push(1);
+        vis.insert(1);
+        vector<int> factors = {2,3,5};
+
+        long curr_ugly = 1;
+        for(int i = 0; i < n; i++){
+            curr_ugly = pq.top();
+            pq.pop();
+
+            for(int f: factors){
+                long next_ugly = curr_ugly * f;
+                if(vis.find(next_ugly) == vis.end()){
+                    pq.push(next_ugly);
+                    vis.insert(next_ugly);
+                }
+            }
+        }
+
+        return (int)curr_ugly;
+    }
+};
+
+// Multiplying an ugly number by 2, 3, or 5 will still yield an ugly number (since its prime factors are only 2, 3, or 5).
