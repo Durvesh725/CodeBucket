5e70e4c98cbd89d6cd52316cf92ab7ea64117086
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-08-19 14:53:20 +0530

    Time: 59 ms (5.18%) | Memory: 20.8 MB (53.00%) - LeetSync

---
+++ LeetCode/749-shortest-completing-word/shortest-completing-word.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string shortestCompletingWord(string l, vector<string>& words) {
+        unordered_map<char, int> um;
+        unordered_map<char, int> temp;
+
+        int n = l.size();
+
+        for(int i = 0; i < n; i++){
+            if(l[i] >= 'a' && l[i] <= 'z'){
+                um[l[i]]++;
+            }
+
+            if(l[i] >= 'A' && l[i] <= 'Z'){
+                char ch = tolower(l[i]);
+                um[ch]++;
+            }
+        }
+
+        int shortestLen = INT_MAX;
+        string ans = "";
+
+        for(int i = 0; i < words.size(); i++){
+            temp = um;
+            string w = words[i];
+            // cout << w << endl;
+            for(auto it: w){
+                if(temp[it] > 1){
+                    temp[it]--;
+                    // if(temp[it] == 0)
+                        
+                }
+                else{
+                    temp.erase(it);
+                }
+            }
+
+            
+            cout << temp.size() << endl;
+            for(auto it: temp){
+                cout << it.first << " " << it.second;
+            }cout << endl;
+
+            if(temp.size() == 0){
+                if(w.size() < shortestLen){
+                    shortestLen = w.size();
+                    ans = w;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
