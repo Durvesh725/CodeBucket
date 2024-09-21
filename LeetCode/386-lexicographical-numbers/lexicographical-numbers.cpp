9a2daca4098f28af37900954dd49decbb6ebb5af
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-09-21 12:16:34 +0530

    Time: 4 ms (88.45%) | Memory: 12.2 MB (95.58%) - LeetSync

---
+++ LeetCode/386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        int curr = 1;
+        vector<int> ans;
+        
+        for(int i = 0; i < n; i++){
+            ans.push_back(curr);
+            if(curr * 10 <= n){
+                curr = curr * 10;
+            }
+            else{
+                if(curr >= n){
+                    curr = curr / 10;
+                }
+                curr++;
+                while(curr % 10 == 0){
+                    curr = curr / 10;
+                }
+            }
+        }
+        return ans;
+    }
+};
