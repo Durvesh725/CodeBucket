3adde66995bc4c948648ec5487f0259d97e9f8a8
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-09-22 09:53:48 +0530

    Time: 724 ms (6.67%) | Memory: 365.8 MB (6.67%) - LeetSync

---
+++ LeetCode/3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_map<string, int> um1;
+        unordered_map<string, int> um2;
+        int count = 0;
+
+        for(string &s: message){
+            um1[s]++;
+        }
+
+        for(string &s: bannedWords){
+            um2[s]++;
+        }
+
+        for(auto it: um1){
+            if(um2.find(it.first) != um2.end()){
+                count += it.second;
+            }
+        }
+
+        if(count >= 2)
+            return true;
+        return false;
+    }
+};
