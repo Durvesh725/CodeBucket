a6959262d5577a960f101189a1fec8118f039986
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-10-28 14:29:34 +0530

    Time: 359 ms (7.62%) | Memory: 13.3 MB (35.66%) - LeetSync

--- LeetCode/16-3sum-closest/3sum-closest.cpp
+++ LeetCode/16-3sum-closest/3sum-closest.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
-        int minDiff = INT_MAX;
-        int ans = 0;
-
-        for(int i = 0; i <= n - 3; i++){
-            for(int j = i+1; j < n; j++){
-                for(int k = j + 1; k < n; k++){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    int diff = abs(target - sum);
-                    if(diff < minDiff){
-                        minDiff = diff;
-                        ans = sum;
-                    }
+        sort(nums.begin(), nums.end());
+     
+        int ans, diff = INT_MAX;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<i; j++){
+                if(j-1 < 0) continue;
+                int a = *lower_bound(nums.begin(), nums.begin()+j-1, target-nums[i]-nums[j]-1);
+                int b = *upper_bound(nums.begin(), nums.begin()+j-1, target-nums[i]-nums[j]-1);
+                int c = *lower_bound(nums.begin(), nums.begin()+j-1, target-nums[i]-nums[j]);
+                int d = *upper_bound(nums.begin(), nums.begin()+j-1, target-nums[i]-nums[j]);
+                
+                if(abs(a+nums[i]+nums[j]-target) < diff){
+                    diff = abs(a+nums[i]+nums[j]-target);
+                    ans = a+nums[i]+nums[j];
+                }
+                if(abs(b+nums[i]+nums[j]-target) < diff){
+                    diff = abs(b+nums[i]+nums[j]-target);
+                    ans = b+nums[i]+nums[j];
+                }
+                if(abs(c+nums[i]+nums[j]-target) < diff){
+                    diff = abs(c+nums[i]+nums[j]-target);
+                    ans = c+nums[i]+nums[j];
+                }
+                if(abs(d+nums[i]+nums[j]-target) < diff){
+                    diff = abs(d+nums[i]+nums[j]-target);
+                    ans = d+nums[i]+nums[j];
                 }
             }
         }
-
+        
         return ans;
     }
 };
