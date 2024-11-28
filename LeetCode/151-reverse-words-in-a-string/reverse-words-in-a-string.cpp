44c7db0bc2e994277c19ffa05e07317446f92971
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-11-28 11:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (52.93%) - LeetSync

--- LeetCode/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ LeetCode/151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string word;
-        stack<string> st;
-
-        while(ss >> word){
-            st.push(word);
-        }
-
+        reverse(s.begin(), s.end());
+        int start = 0, end = 0, n = s.size();
         string res = "";
-        while(!st.empty()){
-            res += st.top() + " ";
-            st.pop();
-        }
 
-        if(res != ""){
-            res.pop_back();
+        while(start < n){
+            while(start < n && s[start] == ' ')
+                start++;
+
+            if(start >= n)  break;
+
+            end = start;
+            while(end < n && s[end] != ' ')
+                end++;
+            
+            if(!res.empty()){
+                res += " ";
+            }
+            string word = s.substr(start, end-start);
+            reverse(word.begin(), word.end());
+            res += word;
+
+            start = end;
+
+            // cout << start << " " << end << endl;
         }
 
         return res;
