994817464cf866148896c9800ee8370505a02486
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-12-01 13:38:14 +0530

    Time: 3 ms (24.95%) | Memory: 13.9 MB (15.21%) - LeetSync

--- LeetCode/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ LeetCode/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -6,14 +6,11 @@ public:
 
         for(int i = 0; i < n; i++){
             int mul = arr[i] * 2;
-            if(st.find(mul) != st.end()){
+
+            if(st.find(mul) != st.end() || (arr[i]%2 == 0 && st.find(arr[i]/2) != st.end())){
                 return true;
             }
-
-            if(arr[i]%2 == 0 && st.find(arr[i]/2) != st.end()){
-                return true;
-            }
-
+            
             st.insert(arr[i]);
         }
 
