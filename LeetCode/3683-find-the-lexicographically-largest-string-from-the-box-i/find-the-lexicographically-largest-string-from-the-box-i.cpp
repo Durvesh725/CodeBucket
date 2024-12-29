4e97ae2b9d4949f0e3fd0012bb415d3664024c90
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2024-12-29 15:29:01 +0530

    Time: 73 ms (84.62%) | Memory: 169.1 MB (46.15%) - LeetSync

---
+++ LeetCode/3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if(numFriends == 1)
+            return word;
+        
+        int n = word.size();
+        int len = n - (numFriends - 1);
+        
+        string largest = "";
+        for(int i = 0; i < n; i++){
+            string s = word.substr(i, len);
+            if(s > largest)
+                largest = s;
+        }
+
+        return largest;
+    }
+};
