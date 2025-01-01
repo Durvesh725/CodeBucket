f54f4769640fea1bfa7e92601b40a378ea1176b5
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2025-01-01 20:56:39 +0530

    Time: 3 ms (38.82%) | Memory: 17.6 MB (6.24%) - LeetSync

--- LeetCode/90-subsets-ii/subsets-ii.cpp
+++ LeetCode/90-subsets-ii/subsets-ii.cpp
@@ -1,23 +1,32 @@
 class Solution {
-public:
-    void solve(int ind, vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp){
-        ans.push_back(temp);
-
-        for(int i = ind; i < nums.size(); i++){
-            if(i > ind && nums[i] == nums[i-1])
-                continue;
-
-            temp.push_back(nums[i]);
-            solve(i + 1, nums, ans, temp);
-            temp.pop_back();
+private:
+    void solve(vector<int>& nums,  set<vector<int>> &ans, vector<int> output, int index){
+        if(index >= nums.size()){
+            //special case if elements inside vector are same but order is changed
+            // sort(output.begin(), output.end());
+            ans.insert(output);
+            return;
         }
-    }
 
+        //exclude
+        solve(nums, ans, output, index+1);
+
+        //include
+        output.push_back(nums[index]);
+        solve(nums, ans, output, index+1);
+    }
+public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> temp;
-        solve(0, nums, ans, temp);
-        return ans;
+        set<vector<int>> ans;
+        vector<int> output;
+        vector<vector<int>> finall;
+        int index = 0;
+        solve(nums, ans, output, index);   
+
+        for(auto i: ans){
+            finall.push_back(i);
+        }
+        return finall;
     }
 };
