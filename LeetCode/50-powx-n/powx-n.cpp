959bcb2b9f4061b191aa1b61c690308687e29397
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2025-01-01 19:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (45.75%) - LeetSync

--- LeetCode/50-powx-n/powx-n.cpp
+++ LeetCode/50-powx-n/powx-n.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    double solve(double x, long n){
+    double solve(double x, long long n){
         if(n == 0)
             return 1;
         
-        if(n < 0)
-            return solve(1/x, -n);
+        // if(n < 0)
+        //     return solve(1/x, -n);
 
         if(n % 2 == 0)
             return solve(x * x, n/2);
@@ -14,6 +14,11 @@ public:
     }
 
     double myPow(double x, int n) {
-        return solve(x, long(n));
+        long long m = n;
+        m = (m < 0) ? -1 * m : n;
+        double ans = solve(x, m);
+        if (n < 0) 
+            return ans = (1.0) / (double)(ans);
+        return ans;
     }
 };
