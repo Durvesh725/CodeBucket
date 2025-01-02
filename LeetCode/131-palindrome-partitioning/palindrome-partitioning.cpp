8ce7024c2f631bd9f76f99690cb63167e99a64a2
Durvesh Jagtap <81422834+Durvesh725@users.noreply.github.com>
2025-01-02 09:54:21 +0530

    Time: 28 ms (96.09%) | Memory: 53.4 MB (80.02%) - LeetSync

--- LeetCode/131-palindrome-partitioning/palindrome-partitioning.cpp
+++ LeetCode/131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int i, int j, string &s){
         while(i <= j){
-            if(s[i] != s[j]){
+            if(s[i] != s[j])
                 return false;
-            }
             i++, j--;
         }
         return true;
     }
 
-    void solve(int ind, string s, vector<vector<string>> &ans, vector<string> temp){
+    void solve(int ind, string &s, vector<vector<string>> &ans, vector<string> &temp){
         if(ind == s.size()){
             ans.push_back(temp);
             return;
